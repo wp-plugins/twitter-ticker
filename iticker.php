ba041c8b8d6d16d526c2a45a1d740ec08db1ad53
osahyoun <osahyoun@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-11 21:30:33 +0000

    new updates
    git-svn-id: https://plugins.svn.wordpress.org/twitter-ticker/trunk@358962 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- iticker.php
+++ iticker.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: iticker - The information ticker
 Plugin URI: http://www.itickerapp.com
-Version: v1.2
+Version: v1.2.2
 Author: iticker
-Description: ITICKER toolbar for websites and blogs: Free, fun and customisable bar to enhance and promote your content. Opt-in to earn with iticker ads and rewards.
+Description: iTICKER toolbar for websites and blogs: Free, fun and customisable bar to enhance and promote your content. Opt-in to earn with iTICKER ads and rewards
 
- 
-Copyright 2011  ITICKER  (email : iticker [a t ] itickerapp DOT com)
+
+Copyright 2011  iTICKER  (email : support [a t ] itickerapp DOT com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -26,18 +26,17 @@ Copyright 2011  ITICKER  (email : iticker [a t ] itickerapp DOT com)
 
 if( !is_admin() ) {
    add_action('wp_footer', 'insert_js');
-
 }
 add_action('admin_menu', 'iticker_config_page');
 
 function iticker_filter_footer() {
   $url = "http://static.itickerapp.com/iticker_seed.js";
   $iticker_sid = get_option('ItickerSID');
-	$iticker_enabled = get_option('ItickerEnabled');	
+  $iticker_enabled = get_option('ItickerEnabled');
 
-	if ($iticker_sid != '' and $iticker_enabled) {
-		wp_enqueue_script('itickerToolbar', $url, false, false, true );
-	}
+  if ($iticker_sid != '' and $iticker_enabled) {
+    wp_enqueue_script('itickerToolbar', $url, false, false, true );
+  }
 }
 
 function insert_js() {
@@ -46,7 +45,7 @@ function insert_js() {
   $c = "</script>";
   echo $a;
   echo $b;
-  echo $c;  
+  echo $c;
 
 echo <<<Javascript
 
@@ -57,62 +56,62 @@ Javascript;
 }
 
 function iticker_config_page() {
-	add_submenu_page('themes.php', __('ITICKER Configuration'), __('ITICKER Configuration'), 'manage_options', 'iticker-key-config', 'iticker_config');
+  add_submenu_page('themes.php', __('ITICKER Configuration'), __('ITICKER Configuration'), 'manage_options', 'iticker-key-config', 'iticker_config');
 }
 
 function iticker_config() {
-	$iticker_sid = get_option('ItickerSid');
-	$iticker_enabled = get_option('ItickerEnabled');
+  $iticker_sid = get_option('ItickerSid');
+  $iticker_enabled = get_option('ItickerEnabled');
 
-	if ( isset($_POST['submit']) ) {
-		if (isset($_POST['sid']))
-		{
-			$iticker_sid = $_POST['sid'];
-			if ($_POST['iticker_enabled'] == 'on')
-			{
-				$iticker_enabled = 1;
-			}
-			else
-			{
-				$iticker_enabled = 0;
-			}
-		}
-		else
-		{
-			$iticker_toolbarpath = '';
-			$iticker_enabled = 0;
-		}
-		update_option('ItickerSid', $iticker_sid);
-		update_option('ItickerEnabled', $iticker_enabled);
-		echo "<div id=\"updatemessage\" class=\"updated fade\"><p>iticker settings updated.</p></div>\n";
-		echo "<script type=\"text/javascript\">setTimeout(function(){jQuery('#updatemessage').hide('slow');}, 3000);</script>";	
-	}
-	?>
-	<div class="wrap">
-		<h2>iticker for WordPress Configuration</h2>
-		<div class="postbox-container">
-			<div class="metabox-holder">	
-				<div class="meta-box-sortables">
-					<form action="" method="post" id="">
-					<div id="" class="postbox">
-						<div class="handlediv" title="Click to toggle"><br /></div>
-						<h3 class="hndle"><span>ITICKER Settings</span></h3>
-						<div class="inside">
-							<table class="form-table">
-								<tr><th valign="top" scrope="row">ITICKER On/Off:</th>
-								<td valign="top"><input type="checkbox" id="iticker_enabled" name="iticker_enabled" <?php echo ($iticker_enabled ? 'checked="checked"' : ''); ?> /> <label for="iticker_enabled">Enable or disable iticker</label><br/></td></tr>
-								<tr><th valign="top" scrope="row"><label for="toolbarpath">ITICKER ID:</label></th>
-								<td valign="top"><input id="sid" name="sid" type="text" size="20" value="<?php echo $iticker_sid; ?>"/></td></tr>
-								<tr><td colspan='2'><p>Get your <strong>ITICKER ID</strong> from <a href="http://www.itickerapp.com" target="_blank">www.itickerapp.com</a></p></td></tr>
-							</table>
-						</div>
-					</div>
-					<div class="submit"><input type="submit" class="button-primary" name="submit" value="Update ITICKER &raquo;" /></div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-	<?php
-} 
+  if ( isset($_POST['submit']) ) {
+    if (isset($_POST['sid']))
+    {
+      $iticker_sid = $_POST['sid'];
+      if ($_POST['iticker_enabled'] == 'on')
+      {
+        $iticker_enabled = 1;
+      }
+      else
+      {
+        $iticker_enabled = 0;
+      }
+    }
+    else
+    {
+      $iticker_toolbarpath = '';
+      $iticker_enabled = 0;
+    }
+    update_option('ItickerSid', $iticker_sid);
+    update_option('ItickerEnabled', $iticker_enabled);
+    echo "<div id=\"updatemessage\" class=\"updated fade\"><p>iticker settings updated.</p></div>\n";
+    echo "<script type=\"text/javascript\">setTimeout(function(){jQuery('#updatemessage').hide('slow');}, 3000);</script>";
+  }
+  ?>
+  <div class="wrap">
+    <h2>iticker for WordPress Configuration</h2>
+    <div class="postbox-container">
+      <div class="metabox-holder">
+        <div class="meta-box-sortables">
+          <form action="" method="post" id="">
+          <div id="" class="postbox">
+            <div class="handlediv" title="Click to toggle"><br /></div>
+            <h3 class="hndle"><span>iTICKER Settings</span></h3>
+            <div class="inside">
+              <table class="form-table">
+                <tr><th valign="top" scrope="row">ITICKER On/Off:</th>
+                <td valign="top"><input type="checkbox" id="iticker_enabled" name="iticker_enabled" <?php echo ($iticker_enabled ? 'checked="checked"' : ''); ?> /> <label for="iticker_enabled">Enable or disable iticker</label><br/></td></tr>
+                <tr><th valign="top" scrope="row"><label for="toolbarpath">iTICKER ID:</label></th>
+                <td valign="top"><input id="sid" name="sid" type="text" size="20" value="<?php echo $iticker_sid; ?>"/></td></tr>
+                <tr><td colspan='2'><p>Get your <strong>ITICKER ID</strong> from <a href="http://www.itickerapp.com" target="_blank">www.itickerapp.com</a></p></td></tr>
+              </table>
+            </div>
+          </div>
+          <div class="submit"><input type="submit" class="button-primary" name="submit" value="Update iTICKER &raquo;" /></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+}
 ?>
--- readme.txt
+++ readme.txt
@@ -6,14 +6,13 @@ Requires at least: 1.5
 Tested up to: 2.8.4
 Stable tag: trunk
 
-iticker is a stylish, customisable information toolbar for your website or blog.
-
+iTICKER toolbar for websites and blogs: Free, fun and customisable bar to enhance and promote your content. Opt-in to earn with iTICKER ads and rewards
 == Description ==
 
 
 == Installation ==
 
-1. Visit <a href="http://www.itickerapp.com" target="_blank">www.itickerapp.com</a> and signin with Twitter to create and configure your iticker toolbar. 
+1. Visit <a href="http://itickerapp.com" target="_blank">itickerapp.com</a> and signin with Twitter to create and configure your iTICKER toolbar.
 
 2. Upload `iticker.php` to the `/wp-content/plugins/` directory
 
@@ -21,28 +20,29 @@ iticker is a stylish, customisable information toolbar for your website or blog.
 
 4. From the Appearance menu, select ITICKER Configuration and enter your ITICKER ID. Get your ITICKER ID by signing in at <a href="http://www.itickerapp.com" target="_blank">www.itickerapp.com</a>, clicking on 'My iticker', selecting 'install' and then 'Wordpress' from the list of supported platforms. Your ID will be in the instructions.
 
-Follow <a href="http://twitter.com/itickerapp" target="_blank">ITICKER</a> to stay up-to-date with new developments.
+Follow <a href="http://twitter.com/itickerapp" target="_blank">iTICKER</a> to stay up-to-date with new developments.
 
 == Screenshots ==
 
 1. Add only the apps you want.
 2. Style to suit your website.
 3. Show off your photo stream.
-4. The iticker bar when collapsed.
+4. The iTICKER bar when collapsed.
 
 == FAQ ==
 Contact us at support [at] itickerapp [dot] com if you need any help installing the plugin or have any feature requests.
 
 == Changelog ==
 <ul>
-  <li>18 July 2010: New iticker widget.</li>
-  <li>20 July 2010: Update documentation.</li>  
-  <li>23 July 2010: Update documentation.</li>    
-  <li>12 September 2010: Update documentation. FLickr Module added.</li>    
-  <li>14 September 2010: Minor bugs fixed.</li>      
+  <li>18 July 2010: New iTICKER widget.</li>
+  <li>20 July 2010: Update documentation.</li>
+  <li>23 July 2010: Update documentation.</li>
+  <li>12 September 2010: Update documentation. FLickr Module added.</li>
+  <li>14 September 2010: Minor bugs fixed.</li>
   <li>11 October 2010: Update description.</li>
   <li>1 December 2010: Update description and add new screen shots.</li>
-  <li>30 January 2011: Large update to flickr module.</li>  
-  <li>18 February 2011: Introduce new icons and twitter follow button.</li>    
-  <li>21 February 2011: Minor styling issues resolved.</li>      
+  <li>30 January 2011: Large update to flickr module.</li>
+  <li>18 February 2011: Introduce new icons and twitter follow button.</li>
+  <li>21 February 2011: Minor styling issues resolved.</li>
+  <li>11 March 2011: New features for Twitter App.</li>
 </ul>
